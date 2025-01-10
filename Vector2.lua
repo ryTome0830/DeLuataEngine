cd3ff498cc35b27536f3ae6306a20009aed52050
ryTome0830 <138864224+ryTome0830@users.noreply.github.com>
2025-01-10 15:24:55 +0900

    DeLuataEngineの構造とObject, Vector2の作成

---
+++ Vector2.lua
@@ -0,0 +1,119 @@
+--- @class Vector2 Vector2クラス
+local Vector2 = {}
+Vector2.__index = Vector2
+
+--- Vector2コンストラクタ
+--- @param x? number|nil x座標
+--- @param y? number|nil y座標
+--- @return Vector2
+function Vector2.new(x, y)
+    --- @class Vector2
+    local instance = setmetatable({}, Vector2)
+    instance:init(x or 0, y or 0)
+    return instance
+end
+
+
+--- Vector2クラス初期化処理
+--- @private
+--- @param x number x座標(default: 0)
+--- @param y number y座標(default: 0)
+function Vector2:init(x, y)
+    self.x = x
+    self.y = y
+end
+
+-- ========== metamethod ==========
+
+--- ベクトルの加算
+--- @param v2 Vector2 加算するベクトル
+--- @return Vector2
+function Vector2:__add(v2)
+    if not v2 then
+        error("Invalid arguments for Vector2 addition")
+    end
+    return Vector2.new(self.x + v2.x, self.y + v2.y)
+end
+
+--- ベクトルの減算
+--- @param v2 Vector2 減算するベクトル
+--- @return Vector2
+function Vector2:__sub(v2)
+    if not v2 then
+        error("Invalid arguments for Vector2 subtraction")
+    end
+    return Vector2.new(self.x - v2.x, self.y - v2.y)
+end
+
+--- ベクトルの等価判定
+--- @param v2 Vector2 比較するベクトル
+--- @return boolean
+function Vector2:__eq(v2)
+    if not v2 then
+        error("Invalid arguments for Vector2 comparison")
+    end
+    return self.x == v2.x and self.y == v2.y
+end
+
+--- ベクトルの文字列化
+function Vector2:__tostring()
+    if not self then
+        error("Invalid argument for Vector2 tostring")
+    end
+    return string.format("Vector2: (%f, %f)", self.x, self.y)
+end
+
+
+-- ========== DeLuataEngine ==========
+
+--- ベクトルのスカラー倍
+--- @param scalar number
+--- @return Vector2
+function Vector2:scale(scalar)
+    return Vector2.new(self.x * scalar, self.y * scalar)
+end
+
+
+--- 内積
+--- @param v Vector2
+--- @return number
+function Vector2:dot(v)
+    return self.x * v.x + self.y * v.y
+end
+
+--- 外積
+--- @param v Vector2
+--- @return number
+function Vector2:cross(v)
+    return self.x * v.y - self.y * v.x
+end
+
+--- ベクトルの大きさ
+--- @return number
+function Vector2:length()
+    if not self then
+      error("Invalid argument for Vector2 length")
+    end
+    return math.sqrt(self.x ^ 2 + self.y ^ 2)
+end
+
+--- 2つのベクトル間の距離
+--- @param v Vector2
+--- @return number
+function Vector2:distance(v)
+    return (self - v):length()
+end
+
+--- ベクトルの回転
+--- @param angle number ラジアン
+--- @return Vector2
+function Vector2:rotate(angle)
+    local cos = math.cos(angle)
+    local sin = math.sin(angle)
+    return Vector2.new(self.x * cos - self.y * sin, self.x * sin + self.y * cos)
+end
+
+
+return {
+    Vector2=Vector2,
+}
---
+++ abstruct/Object.lua
@@ -0,0 +1,75 @@
+--[[ 
+This Object implementation is based on the one from SNKRX, 
+which is licensed under the MIT License. 
+SNKRX is an open-source game available at (https://github.com/a327ex/SNKRX/blob/master/engine/game/object.lua).
+This class has been slightly modified for use in this project.
+This Object base class implementation was taken from SNKRX (MIT license)
+]]
+
+--- @class Object
+local Object = {}
+Object.__index = Object
+
+--- コンストラクタ
+--- @return Object
+function Object.new()
+    --- @class Object
+    local instance = setmetatable({}, Object)
+    instance:init()
+    return instance
+end
+
+--- 初期化処理
+--- @private
+function Object:init()
+end
+
+
+-- ========== metamethod ==========
+
+--- 
+--- @return string
+function Object:__tostring()
+    return "Object"
+end
+
+
+-- ========== DeLuataEngine ==========
+
+--- Objectクラスの継承
+--- @return table
+function Object:extend()
+    -- 新しいクラスclsを作成
+    local cls = {}
+    -- Objectクラスの'__'で始まるプロパティをコピー
+    for k, v in pairs(self) do
+        if k:find("__") == 1 then
+        cls[k] = v
+        end
+    end
+    -- clsのメタテーブル__indexにclsを設定
+    --cls.__index = cls
+    -- 親クラスの参照を保持
+    cls.super = self
+    -- cls
+    setmetatable(cls, self)
+    return cls
+end
+
+--- インスタンスがクラスTのインスタンスまたはそのサブクラスのインスタンスかどうかを判定
+--- @param T table クラス
+--- @return boolean
+function Object:is(T)
+    local mt = getmetatable(self)
+    while mt do
+        if mt == T then
+            return true
+        end
+        mt = getmetatable(mt)
+    end
+    return false
+end
+
+return {
+    Object=Object
+}
